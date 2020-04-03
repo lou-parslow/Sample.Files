f863c6b90ceff14f45fe54c307b0e25321822ef3
Lou <lou.parslow@gmail.com>
2020-04-03 08:57:11 -0600

    integrate

--- rakefile.rb
+++ rakefile.rb
@@ -3,8 +3,8 @@ require 'dotkit'
 task :default do
 	#PROJECT.info
 	#target='Sample.Files.Test/obj/Release/netcoreapp2.0/Sample.Files.Test.csproj.CopyComplete'
-	CLEAN.exclude(target)
-	if(PROJECT.last_modified_filename != target)
+	#CLEAN.exclude(target)
+	#if(PROJECT.last_modified_filename != target)
 		#PROJECT.run(["dotnet build --configuration Release",
 		#			"dotnet test #{PROJECT.name}.Test/#{PROJECT.name}.Test.csproj -c Release -v normal",
 		#			"dotnet pack #{PROJECT.name}.sln -c Release"])
@@ -15,6 +15,6 @@ task :default do
 		#PROJECT.run("dotnet nuget push #{PROJECT.name}/bin/Release/#{PROJECT.name}.#{PROJECT.version}.nupkg -k #{NUGET_KEY} -s https://api.nuget.org/v3/index.json",false)
 
 		#PROJECT.commit.tag.push.pull
-	end
+	#end
 	#PROJECT.summary
 end
